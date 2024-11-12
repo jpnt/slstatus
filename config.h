904da2bffdc006e9f734e4a29d1779a22135c317
Joao Pinto <joaopintojpgp@gmail.com>
2024-11-12 18:47:51 +0000

    show ipv4

--- config.h
+++ config.h
@@ -65,9 +65,10 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ cpu_perc, "[CPU: %s%%] ",	NULL },
-	{ ram_perc, "[RAM: %s%%] ",	NULL },
-	{ battery_state, "[BAT(%s): ",	"BAT0" },
-	{ battery_perc, "%s%%] ",	"BAT0" },
+	{ ipv4, "{IPv4: %s} ",          "wlo1" },
+	{ cpu_perc, "{CPU: %s%%} ",	NULL },
+	{ ram_perc, "{RAM: %s%%} ",	NULL },
+	{ battery_state, "{BAT(%s): ",	"BAT0" },
+	{ battery_perc, "%s%%} ",	"BAT0" },
 	{ datetime, "%s",		"%a %b %d %R" },
 };
